4e2f1465fb4ad0d8d9251d36487adeb93daaaca5
Justin Cox <standardamericanevisceration@gmail.com>
2024-11-13 16:25:36 -0500

    4th commit

--- src/main/java/org/example/trackFile.java
+++ src/main/java/org/example/trackFile.java
@@ -3,8 +3,9 @@ package org.example;
 public class trackFile {
     public static void main(String[] args) {
         System.out.println("Does not matter. TRACKING!");
-        System.out.println("This is project version 3.");
+        System.out.println("This is project version 4.");
         System.out.println("Holy Hell it's on fire!");
         System.out.println("Another change.");
+        System.out.println("AAAAAAAAHH!");
     }
 }
