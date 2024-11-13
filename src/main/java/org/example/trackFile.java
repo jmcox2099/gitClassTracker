6e89620143279a50ee62f3c525e8c77103f937c5
Justin Cox <standardamericanevisceration@gmail.com>
2024-11-13 16:05:09 -0500

    secondCommit

--- src/main/java/org/example/trackFile.java
+++ src/main/java/org/example/trackFile.java
@@ -3,6 +3,7 @@ package org.example;
 public class trackFile {
     public static void main(String[] args) {
         System.out.println("Does not matter. TRACKING!");
-        System.out.println("This is project version 1.");
+        System.out.println("This is project version 2.");
+        System.out.println("Holy Hell it's on fire!");
     }
 }
