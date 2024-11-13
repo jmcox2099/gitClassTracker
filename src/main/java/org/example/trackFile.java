be4ddfb2226649062a82fd1c48e34f8b61f73cb2
Justin Cox <standardamericanevisceration@gmail.com>
2024-11-13 15:45:44 -0500

    1stName

---
+++ src/main/java/org/example/trackFile.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class trackFile {
+    public static void main(String[] args) {
+        System.out.println("Does not matter. TRACKING!");
+        System.out.println("This is project version 1.");
+    }
+}
